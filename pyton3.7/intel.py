60cc52069098794be0daa13cffdda8603e13de68
nikita <nikitaganchukov@bk.ru>
2019-10-16 11:59:30 +0700

    upgared intel.py

--- pyton3.7/intel.py
+++ pyton3.7/intel.py
@@ -1,1 +1,41 @@
-print("Hello")
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+training_inputs = np.array([[0, 0, 1],
+                           [1, 1, 1],
+                           [1, 0, 1],
+                           [0, 1, 1]])
+
+training_outputs = np.array([[0, 1, 1, 0]]).T
+
+np.random.seed(1)
+
+synaptic_weights = 2 * np.random.random((3,1)) - 1
+
+print("ddd:")
+print(synaptic_weights)
+
+# метод обратного распространения
+for i in range(20000):
+    input_layer = training_inputs
+    outputs = sigmoid( np.dot(input_layer, synaptic_weights) )
+
+    err = training_outputs - outputs
+    adjustment = np.dot( input_layer.T, err * (outputs * (1 - outputs)) )
+
+    synaptic_weights += adjustment
+
+print("hh")
+print(synaptic_weights)
+
+print("gg")
+print(outputs)
+
+# Test
+new_inputs = np.array([1,1,0]) # New ситуация
+outputs = sigmoid( np.dot(new_inputs, synaptic_weights) )
+
+print("new:")
+print(outputs)
